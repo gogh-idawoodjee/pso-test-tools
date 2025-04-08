dcaa65c0054da98e0723177d866ed949552a04d7
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-08 15:46:44 -0400

    added artisan clear log command

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,9 @@ Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('logs:clear', function () {
+    exec('rm -f ' . storage_path('logs/*.log'));
+    exec('rm -f ' . base_path('*.log'));
+    $this->comment('Logs have been cleared!');
+})->describe('Clear log files');
