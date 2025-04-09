e2d8fa6928469d7cf7e7dfcc7b6a7176f5c829ff
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-09 19:59:14 -0400

    create exception page

---
+++ app/Filament/Pages/GenerateCustomException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class GenerateCustomException extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.generate-custom-exception';
+}
