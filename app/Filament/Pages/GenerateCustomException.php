7889a5c12365638f02beaf6e9cdde05c62735f02
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-09 20:09:08 -0400

    structure

--- app/Filament/Pages/GenerateCustomException.php
+++ app/Filament/Pages/GenerateCustomException.php
@@ -2,11 +2,33 @@
 
 namespace App\Filament\Pages;
 
+use App\Classes\PSOObjectRegistry;
+use App\Enums\HttpMethod;
+use App\Models\Environment;
+use App\Traits\FormTrait;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Form;
+use Filament\Forms;
+use Filament\Forms\Get;
 use Filament\Pages\Page;
+use Illuminate\Support\Str;
+use JsonException;
+use Override;
 
 class GenerateCustomException extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    use InteractsWithForms, FormTrait;
+
+    protected static ?string $navigationIcon = 'heroicon-o-exclamation-triangle';
+    protected static ?string $activeNavigationIcon = 'heroicon-s-exclamation-triangle';
 
     protected static string $view = 'filament.pages.generate-custom-exception';
+    protected static ?string $navigationGroup = 'Additional Tools';g
+
 }
