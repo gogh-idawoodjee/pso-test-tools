41858cd7e7ac3448afc05033c8de90e8c858d869
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-04-30 19:00:10 -0400

    set ID to uuid

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -4,21 +4,25 @@ namespace Database\Seeders;
 
 use App\Models\Region;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class RegionSeeder extends Seeder
 {
     public function run(): void
     {
         $regions = [
-            ['id' => 'SVBARR', 'name' => 'Service Barrie'],
-            ['id' => 'OTWEST', 'name' => 'Ottawa West'],
-            ['id' => 'INWIND', 'name' => 'Interior Windsor'],
-            ['id' => 'METRSC', 'name' => 'Metro Scarborough'],
-            ['id' => 'CAP01',  'name' => 'Capital Region'],
+            'Service Barrie',
+            'Ottawa West',
+            'Interior Windsor',
+            'Metro Scarborough',
+            'Capital Region',
         ];
 
-        foreach ($regions as $region) {
-            Region::create($region);
+        foreach ($regions as $name) {
+            Region::create([
+                'id'   => Str::uuid()->toString(),
+                'name' => $name,
+            ]);
         }
     }
 }
