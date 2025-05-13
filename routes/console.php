5dc0a58d920b66f6e4c3741093730fb96550aa87
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-05-13 11:38:44 -0400

    configure backups

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,5 @@ Artisan::command('logs:clear', function () {
 })->describe('Clear log files');
 
 Schedule::command('telescope:prune')->daily();
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
