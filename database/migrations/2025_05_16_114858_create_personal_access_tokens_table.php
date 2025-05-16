673eb90efd34a2410f1d162de1eac61d7e6fa5a5
Ismail Dawoodje - Gogh Solutions <103516145+gogh-idawoodjee@users.noreply.github.com>
2025-05-16 14:09:03 -0400

    using local db instead of remote

--- database/migrations/2025_05_16_114858_create_personal_access_tokens_table.php
+++ database/migrations/2025_05_16_114858_create_personal_access_tokens_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
 
-    public $connection = 'shared_tokens';
+//    public $connection = 'shared_tokens';
 
     /**
      * Run the migrations.
